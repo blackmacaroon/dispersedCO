4c04bf7267f9ed22bf2955edbafa02fa4a89e3c9
Kayla <6lackmacaroon@gmail.com>
2020-01-08 16:17:32 -0700

    connect comment to camp

--- app.js
+++ app.js
@@ -87,12 +87,20 @@ app.post("/campgrounds/:id/comments/", function(req, res){
             console.log(err)
             res.redirect("/campgrounds")
         } else {
-            Comment.create({})
+            // create new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if (err){
+                    console.log(err)
+                } else {
+                    // connect new comment to campground
+                    campground.comments.push(comment)
+                    campground.save()
+                    //redirect campground show page
+                    res.redirect("/campgrounds/" + campground._id)
+                }
+            })
         }
     })
-    // creaete new comment
-    // commect new comment to campground
-    //redirect campground show page
 })
 
 
