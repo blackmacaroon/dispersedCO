b2973f7b62e1b50d07b1833cff2d9c2c6e2d7fae
Kayla <6lackmacaroon@gmail.com>
2020-01-09 16:06:20 -0700

    login working and routing correctly

--- app.js
+++ app.js
@@ -139,9 +139,15 @@ app.post("/register", function(req, res){
 
 // LOGIN ROUTES
 app.get("/login", function(req, res){
-    res.render("login")
+    res.render("login", {page: 'login'})
 })
 
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+})
 
 app.listen(port, err => {
     if (err) console.log(err);
