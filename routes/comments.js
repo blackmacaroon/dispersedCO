9ee3f5f0a409ff1a68c37b0818b02b33a2b6d55c
Kayla <6lackmacaroon@gmail.com>
2020-01-20 21:05:46 -0700

    refactor comment routesstep 1

--- routes/comments.js
+++ routes/comments.js
@@ -1,3 +1,40 @@
 const express = require("express")
 const router = express.Router({mergeParams: true})
 const Campground = require("../models/campground")
+const Comment = require("../models/comment")
+const middleware = require("../middleware")
+const { isLoggedIn, checkUserComment, isAdmin } = middleware
+
+router.get("/new", isLoggedIn, function(req, res){
+    console.log(req.params.id)
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("/comments/new", {campground: campground})
+        }
+    })
+});
+
+router.post("/", isLoggedIn, function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+             console.log(err)
+             res.redirect("/campgrounds")
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err)
+                } else {
+                    comment.author.id = req.user._id
+                    comment.author.username = req.username
+                    comment.save()
+                    campground.comments.push(comment)
+                    campground.save()
+                    console.log(comment)
+                    res.redirect('/campgrounds/' + campground._id)
+                }
+            })
+        }
+    })
+});
