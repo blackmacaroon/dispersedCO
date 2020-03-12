be1198642bde5063e605d18160c43811df97a466
Kayla <6lackmacaroon@gmail.com>
2020-03-12 11:16:47 -0600

    update middleware, remove admin role

--- middleware/index.js
+++ middleware/index.js
@@ -1,44 +1,51 @@
 const Comment = require("../models/comment")
 const Campground = require("../models/campground")
+
 module.exports = {
     isLoggedIn: function(req, res, next){
         if(req.isAuthenticated()){
             return next()
         }
-        res.redirect('/login')
+        req.flash("error", "You must be logged in to do that")
+        res.redirect("/login")
     },
     checkUserCamp: function(req, res, next){
-        Campground.findById(req.params.id, function(err, foundCampground){
-            if(err || !foundCampground){
-                console.log(err)
-                res.redirect('/campground')
-            } else if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
-                req.campground = foundCampground
-                next();
-            } else {
-                res.redirect('/campgrounds/' + req.params.id)
-            }
-        })
+        if(req.isAuthenticated()){
+            Campground.findById(req.params.id, function(err, foundCampground){
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    req.flash("error", "You don't have permission to do that")
+                    console.log("bad!!!")
+                    res.redirect("/campgrounds/" + req.params.id)
+                }
+            })
+        } else {
+            req.flash("error", "You need to be signed in to do that")
+            res.redirect("/login")
+        }
     },
     checkUserComment: function(req, res, next){
-        Comment.findById(req.params.commentId, function(err, foundComment){
-            if(err || !foundComment){
-                console.log(err)
-                res.redirect('/campground')
-            } else if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
-                req.comment = foundComment
-                next()
-            } else {
-                res.redirect('/campgrounds/' + req.params.id)
-            }
-        })
-    },
-    isAdmin: function(req, res, next){
-        if(req.user.isAdmin) {
-            next()
+        if(req.isAuthenticated()){
+            Comment.findById(req.params.commentId, function(err, foundComment){
+    
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next()
+                } else {
+                    req.flash("error", "You don't have permission to do that")
+                    res.redirect("/campgrounds/" + req.params.id)
+                }
+            })
         } else {
-            res.redirect('back')
+
         }
-    }
+    },
+    // isAdmin: function(req, res, next){
+    //     if(req.user.isAdmin) {
+    //         next()
+    //     } else {
+    //         res.redirect('back')
+    //     }
+    // }
     
 }
